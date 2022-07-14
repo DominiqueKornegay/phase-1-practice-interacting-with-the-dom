17759c2e7dfe81d45906dc5a61de4c97af5fbc48
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-07-14 16:38:00 -0400

    commit tree 00013502caed62e6d446a321791c07982333ee4c

---
+++ js/challenge.js
@@ -0,0 +1,10 @@
+const min = document.getElementById('minus')
+
+function minCons () {
+    min.event.addEventListener('click', (event) => {
+        for (var i = 0, i > 0; i--)
+    })
+} 
+
+minCons()
+
